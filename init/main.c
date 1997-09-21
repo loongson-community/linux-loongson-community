ef2ab213cf2601ba4c2dbc32e6fb2e28bf00534b
Miguel de Icaza <miguel@nuclecu.unam.mx>
1997-09-21 23:10:36 +0000

    init the irix inventory.

--- init/main.c
+++ init/main.c
@@ -950,6 +950,7 @@ __initfunc(asmlinkage void start_kernel(void))
 		initrd_start = 0;
 	}
 #endif
+	init_inventory ();
 	mem_init(memory_start,memory_end);
 	kmem_cache_sizes_init();
 #ifdef CONFIG_PROC_FS
