2e36d7a74ec75c24009475b788a3f9bf94f63b4d
Miguel de Icaza <miguel@nuclecu.unam.mx>
1997-09-21 22:25:36 +0000

    IRIX inventory support.  Right now it has hardcoded my machine's information.  This needs to be fixed at least to report the proper graphics capabilities on the machine.  Optimally, we should scatter the right calls to the inventory all over the kernel source.

---
+++ include/asm-mips/inventory.h
@@ -0,0 +1,26 @@
+#ifndef __ASM_MIPS_INVENTORY_H
+#define __ASM_MIPS_INVENTORY_H
+
+#ifdef CONFIG_BINFMT_IRIX
+typedef struct inventory_s {
+	struct inventory_s *inv_next;
+	int    inv_class;
+	int    inv_type;
+	int    inv_controller;
+	int    inv_unit;
+	int    inv_state;
+} inventory_t;
+
+extern int inventory_items;
+void add_to_inventory (int class, int type, int controller, int unit, int state);
+int dump_inventory_to_user (void *userbuf, int size);
+void init_inventory (void);
+
+#else
+#define add_to_inventory(c,t,o,u,s)
+#define init_inventory()
+#endif
+#endif
+
+
+
