f455a7a14127d27774c00a4ae676db3d1675e92f
Ulf Carlsson <md1ulfc@mdstud.chalmers.se>
1999-04-05 01:30:49 +0000

    You need this symbols in order to use ALSA.

--- arch/mips/kernel/mips_ksyms.c
+++ arch/mips/kernel/mips_ksyms.c
@@ -1,4 +1,4 @@
-/* $Id: mips_ksyms.c,v 1.16 1999/02/25 20:36:34 tsbogend Exp $
+/* $Id: mips_ksyms.c,v 1.17 1999/04/01 23:35:16 ulfc Exp $
  *
  * Export MIPS-specific functions needed for loadable modules.
  *
@@ -80,6 +80,9 @@ EXPORT_SYMBOL(csum_partial_copy);
 EXPORT_SYMBOL(flush_page_to_ram);
 EXPORT_SYMBOL(flush_cache_all);
 EXPORT_SYMBOL(dma_cache_wback_inv);
+EXPORT_SYMBOL(dma_cache_inv);
+
+EXPORT_SYMBOL(invalid_pte_table);
 
 /*
  * Base address of ports for Intel style I/O.
