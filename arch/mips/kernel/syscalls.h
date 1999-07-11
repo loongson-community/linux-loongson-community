a2b759bd20319fc590e3f13cc3f24ad21d02b025
Ralf Baechle <ralf@linux-mips.org>
1999-07-11 23:56:29 +0000

    Due to the MIPS calling sequence pread(2) / pwrite(2) effectivly are getting called with 6 arguments from userland.
    This also means that _every_ damn fucking piece of software that
    attempts to do syscalls to these functions in the absence of glibc is
    borken, pciutils just being one example.

--- arch/mips/kernel/syscalls.h
+++ arch/mips/kernel/syscalls.h
@@ -1,4 +1,4 @@
-/* $Id: syscalls.h,v 1.14 1998/08/25 09:14:41 ralf Exp $
+/* $Id: syscalls.h,v 1.15 1998/09/19 19:16:17 ralf Exp $
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
@@ -215,8 +215,8 @@ SYS(sys_rt_sigpending, 2)
 SYS(sys_rt_sigtimedwait, 4)
 SYS(sys_rt_sigqueueinfo, 3)
 SYS(sys_rt_sigsuspend, 2)
-SYS(sys_pread, 4)				/* 4200 */
-SYS(sys_pwrite, 4)
+SYS(sys_pread, 6)				/* 4200 */
+SYS(sys_pwrite, 6)
 SYS(sys_chown, 3)
 SYS(sys_getcwd, 2)
 SYS(sys_capget, 2)
