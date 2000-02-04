5dcbdc26ebc8f68f1f49301f8f7943b744d323c0
Kanoj Sarcar <kanoj@engr.sgi.com>
2000-02-04 08:27:50 +0000

    Fix default compile after the 2.3.32 merge by adding more stubs.

--- arch/mips64/kernel/linux32.c
+++ arch/mips64/kernel/linux32.c
@@ -4,7 +4,4 @@ int sys32_newfstat(void) {return 0;}
 int sys_mmap2(void) {return 0;}
 int sys_truncate64(void) {return 0;}
 int sys_ftruncate64(void) {return 0;}
-int sys_stat64(void) {return 0;}
-int sys_lstat64(void) {return 0;}
-int sys_fstat64(void) {return 0;}
 
