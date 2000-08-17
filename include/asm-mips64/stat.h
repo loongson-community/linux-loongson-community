dd458fcabd000a41fdcf52351c8c7cabd79522c9
Ralf Baechle <ralf@linux-mips.org>
2000-08-17 17:19:09 +0000

    Adapt to struct stat changes from test6-pre2.  Once more glibc 2.2 users will enjoy the priviledge of a full recompile ...

--- include/asm-mips64/stat.h
+++ include/asm-mips64/stat.h
@@ -55,26 +55,36 @@ struct stat32 {
 struct stat {
 	dev_t		st_dev;
 	unsigned int	st_pad0[3];	/* Reserved for st_dev expansion  */
-	ino_t		st_ino;
+
+	unsigned long	st_ino;
+
 	mode_t		st_mode;
 	nlink_t		st_nlink;
+
 	uid_t		st_uid;
 	gid_t		st_gid;
+
 	dev_t		st_rdev;
-	unsigned int	st_pad1[2];	/* Reserved for st_rdev expansion  */
+	unsigned int	st_pad1[3];	/* Reserved for st_rdev expansion  */
+
 	off_t		st_size;
+
 	/*
 	 * Actually this should be timestruc_t st_atime, st_mtime and st_ctime
 	 * but we don't have it under Linux.
 	 */
 	time_t		st_atime;
 	unsigned int	reserved0;	/* Reserved for st_atime expansion  */
+
 	time_t		st_mtime;
-	unsigned int	reserved1;	/* Reserved for st_atime expansion  */
+	unsigned int	reserved1;	/* Reserved for st_mtime expansion  */
+
 	time_t		st_ctime;
-	unsigned int	reserved2;	/* Reserved for st_atime expansion  */
+	unsigned int	reserved2;	/* Reserved for st_ctime expansion  */
+
 	unsigned int	st_blksize;
 	unsigned int	st_pad2;
+
 	unsigned long	st_blocks;
 };
 
