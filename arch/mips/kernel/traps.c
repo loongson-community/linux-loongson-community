9aafbaa2382ffe1909773b07417a5f0f6c63dfb7
Ralf Baechle <ralf@linux-mips.org>
2000-08-22 01:01:15 +0000

    One more RM7000 hook.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -715,6 +715,7 @@ void __init trap_init(void)
 	case CPU_R4600:
         case CPU_R5000:
         case CPU_NEVADA:
+        case CPU_RM7000:
 		if(mips_cputype == CPU_NEVADA) {
 			memcpy((void *)KSEG0, &except_vec0_nevada, 0x80);
 		} else if (mips_cputype == CPU_R4600)
