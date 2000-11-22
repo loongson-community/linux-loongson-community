4a13312b4c9256ff4bc5eba83ac242448bf1cd4b
Ralf Baechle <ralf@linux-mips.org>
2000-11-22 15:51:26 +0000

    Unify the dozen pci-dma copies we had.

--- arch/mips/kernel/pci-dma.c
+++ arch/mips/kernel/pci-dma.c
@@ -1,11 +1,9 @@
 /*
- * Copyright (C) 2000   Ani Joshi <ajoshi@unixbox.com>
- *
  *
  * Dynamic DMA mapping support.
  *
- * swiped from i386, and cloned for MIPS by Geert.
- *
+ * Copyright (C) 2000   Ani Joshi <ajoshi@unixbox.com>
+ * swiped from i386, and cloned for MIPS by Geert, polished by Ralf.
  */
 #include <linux/types.h>
 #include <linux/mm.h>
@@ -13,14 +11,6 @@
 #include <linux/pci.h>
 
 #include <asm/io.h>
-
-/*
- * [jsun] We want to return non-cached area so that data can be consistent
- * Apparently on x86, this is not an issue because cache is automatically
- * invalidated.
- *
- * To make we are doing the right thing, I add some extra debug macros.
- */
 
 void *pci_alloc_consistent(struct pci_dev *hwdev, size_t size,
 			   dma_addr_t * dma_handle)
@@ -36,13 +26,12 @@ void *pci_alloc_consistent(struct pci_dev *hwdev, size_t size,
 		dma_cache_inv((unsigned long) ret, size);
 		*dma_handle = virt_to_bus(ret);
 	}
-	ret = (void *) ((unsigned long) ret | 0x20000000);
-	return ret;
+
+	return KSEG1ADDR(ret);
 }
 
 void pci_free_consistent(struct pci_dev *hwdev, size_t size,
 			 void *vaddr, dma_addr_t dma_handle)
 {
-	vaddr = (void *) ((unsigned long) vaddr & ~0x20000000);
-	free_pages((unsigned long) vaddr, get_order(size));
+	free_pages((unsigned long) KSEG0ADDR(vaddr), get_order(size));
 }
