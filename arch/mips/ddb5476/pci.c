eaf87404ebd5527937100fc4b9ba72035bab7847
Ralf Baechle <ralf@linux-mips.org>
2001-02-05 00:40:54 +0000

    Remove calls to pci_set_bus_ranges which no longer exists.

--- arch/mips/ddb5476/pci.c
+++ arch/mips/ddb5476/pci.c
@@ -347,7 +347,6 @@ void __init pcibios_init(void)
 	pci_scan_bus(0, &nile4_pci_ops, NULL);
 	ddb5476_pci_fixup();
 	pci_assign_unassigned_resources();
-	pci_set_bus_ranges();
 	pcibios_fixup_irqs();
 }
 
