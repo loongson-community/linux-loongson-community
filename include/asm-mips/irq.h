8709bd01cee13852179a1a60f1763904776b3574
Ralf Baechle <ralf@linux-mips.org>
2001-02-24 17:54:29 +0000

    Handle CONFIG_ROTTEN_IRQ correctly.

--- include/asm-mips/irq.h
+++ include/asm-mips/irq.h
@@ -4,7 +4,7 @@
  * for more details.
  *
  * Copyright (C) 1994 by Waldorf GMBH, written by Ralf Baechle
- * Copyright (C) 1995, 96, 97, 98, 99, 2000 by Ralf Baechle
+ * Copyright (C) 1995, 96, 97, 98, 99, 2000, 2001 by Ralf Baechle
  */
 #ifndef _ASM_IRQ_H
 #define _ASM_IRQ_H
@@ -27,7 +27,13 @@ static inline int irq_cannonicalize(int irq)
 struct irqaction;
 extern int i8259_setup_irq(int irq, struct irqaction * new);
 extern void disable_irq(unsigned int);
+
+#ifdef CONFIG_ROTTEN_IRQ
 #define disable_irq_nosync	disable_irq
+#else
+extern void disable_irq_nosync(unsigned int);
+#endif
+
 extern void enable_irq(unsigned int);
 
 /* Machine specific interrupt initialization  */
