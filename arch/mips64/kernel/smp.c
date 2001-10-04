4faf934761b807743ef2b3301800895c7bd8c543
Ralf Baechle <ralf@linux-mips.org>
2001-10-04 03:33:14 +0000

    Add copyright notice.

--- arch/mips64/kernel/smp.c
+++ arch/mips64/kernel/smp.c
@@ -1,3 +1,12 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2000, 2001 Kanoj Sarcar
+ * Copyright (C) 2000, 2001 Ralf Baechle
+ * Copyright (C) 2000, 2001 Silicon Graphics, Inc.
+ */
 #include <linux/config.h>
 #include <linux/init.h>
 #include <linux/spinlock.h>
