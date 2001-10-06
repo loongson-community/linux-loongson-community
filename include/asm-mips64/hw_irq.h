47ca9826f69693dd17272d12cc450824872d9406
Ralf Baechle <ralf@linux-mips.org>
2001-10-06 19:44:36 +0000

    Interrupt numbers used with IP27 SMP.

--- include/asm-mips64/hw_irq.h
+++ include/asm-mips64/hw_irq.h
@@ -1,5 +1,15 @@
-/* This exists merely to satisfy <linux/irq.h>.  There is
-   nothing that would go here of general interest.
+/*
+ * This exists merely to satisfy <linux/irq.h>.  There is
+ * nothing that would go here of general interest.
+ *
+ * Everything of consequence is in arch/alpha/kernel/irq_impl.h,
+ * to be used only in arch/alpha/kernel/.
+ */
 
-   Everything of consequence is in arch/alpha/kernel/irq_impl.h,
-   to be used only in arch/alpha/kernel/.  */
+/*
+ * Special interrupt numbers for SMP
+ *
+ * XXX ATM this is IP27 specific.
+ */
+#define DORESCHED       0xab
+#define DOCALL          0xbc
