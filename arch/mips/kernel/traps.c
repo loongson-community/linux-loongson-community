f0ba8fc8e4b5a58680a580328181554478c10f06
Ralf Baechle <ralf@linux-mips.org>
2001-11-24 17:14:31 +0000

    EJTAG exception handler is located at KSEG0+0x200.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -845,7 +845,7 @@ void __init trap_init(void)
 	 * destination.
 	 */
 	if (mips_cpu.options & MIPS_CPU_EJTAG)
-		memcpy((void *)(KSEG0 + 0x300), &except_vec_ejtag_debug, 0x80);
+		memcpy((void *)(KSEG0 + 0x200), &except_vec_ejtag_debug, 0x80);
 
 	/*
 	 * Only some CPUs have the watch exceptions or a dedicated
