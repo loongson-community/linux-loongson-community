0a97f73b32b9c908e2c3f34bfb2c0603a0da4331
Ralf Baechle <ralf@linux-mips.org>
2001-11-27 07:33:27 +0000

    Remove dead arc_setup_console references.

--- arch/mips/arc/init.c
+++ arch/mips/arc/init.c
@@ -23,8 +23,6 @@ unsigned short prom_vers, prom_rev;
 
 extern void prom_testtree(void);
 
-extern void arc_setup_console(void);
-
 void __init prom_init(int argc, char **argv, char **envp, int *prom_vec)
 {
 	struct linux_promblock *pb;
@@ -35,19 +33,6 @@ void __init prom_init(int argc, char **argv, char **envp, int *prom_vec)
 	prom_argv = argv;
 	prom_envp = envp;
 
-#if 0
-	/* arc_printf should not use prom_printf as soon as we free
-	 * the prom buffers - This horribly breaks on Indys with framebuffer
-	 * as it simply stops after initialising swap - On the Indigo2 serial
-	 * console you will get A LOT illegal instructions - Only enable
-	 * this for early init crashes - This also brings up artefacts of
-	 * printing everything twice on serial console and on GFX Console
-	 * this has the effect of having the prom printing everything
-	 * in the small rectangle and the kernel printing around.
-	 */
-
-	arc_setup_console();
-#endif
 	if (pb->magic != 0x53435241) {
 		prom_printf("Aieee, bad prom vector magic %08lx\n", pb->magic);
 		while(1)
