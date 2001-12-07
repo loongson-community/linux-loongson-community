cabda75fe5ec820d8ed33f5f4f36722cc6a5405f
Ralf Baechle <ralf@linux-mips.org>
2001-12-07 19:32:00 +0000

    C argument evaluation rules ...

--- arch/mips/lib/dump_tlb.c
+++ arch/mips/lib/dump_tlb.c
@@ -197,11 +197,11 @@ dump16(unsigned long *p)
 {
 	int i;
 
-	for(i=0;i<8;i++)
-	{
+	for (i=0;i<8;i++) {
 		printk("*%08lx == %08lx, ",
-		       (unsigned long)p, (unsigned long)*p++);
+		       (unsigned long)p, (unsigned long)p[0]);
 		printk("*%08lx == %08lx\n",
-		       (unsigned long)p, (unsigned long)*p++);
+		       (unsigned long)p, (unsigned long)*p[1]);
+		p += 2;
 	}
 }
