132a85b9e907e2ff57bb894de38f56d2b452997b
Ralf Baechle <ralf@linux-mips.org>
2002-02-25 00:58:31 +0000

    Reformat.

--- include/asm-mips64/pgtable.h
+++ include/asm-mips64/pgtable.h
@@ -125,7 +125,7 @@ extern void (*_flush_cache_l1)(void);
 #define VMALLOC_START     XKSEG
 #define VMALLOC_VMADDR(x) ((unsigned long)(x))
 #define VMALLOC_END       \
-  (VMALLOC_START + ((1 << KPTBL_PAGE_ORDER) * PTRS_PER_PTE * PAGE_SIZE))
+	(VMALLOC_START + ((1 << KPTBL_PAGE_ORDER) * PTRS_PER_PTE * PAGE_SIZE))
 
 /* Note that we shift the lower 32bits of each EntryLo[01] entry
  * 6 bits to the left. That way we can convert the PFN into the
