71c1ebe2d7420937ea7b1b591c9625097fe163a7
Ralf Baechle <ralf@linux-mips.org>
2002-06-28 12:12:14 +0000

    Remove remains of IRIX5 support.  Once we add propper binary compatibility this can be added back but for the moment this just was clutter.

--- arch/mips64/kernel/process.c
+++ arch/mips64/kernel/process.c
@@ -85,16 +85,9 @@ int copy_thread(int nr, unsigned long clone_flags, unsigned long usp,
 	childregs = (struct pt_regs *) childksp - 1;
 	*childregs = *regs;
 	childregs->regs[7] = 0;	/* Clear error flag */
-	if (current->personality == PER_LINUX) {
-		childregs->regs[2] = 0;	/* Child gets zero as return value */
-		regs->regs[2] = p->pid;
-	} else {
-		/* Under IRIX things are a little different. */
-		childregs->regs[2] = 0;
-		childregs->regs[3] = 1;
-		regs->regs[2] = p->pid;
-		regs->regs[3] = 0;
-	}
+	childregs->regs[2] = 0;	/* Child gets zero as return value */
+	regs->regs[2] = p->pid;
+
 	if (childregs->cp0_status & ST0_CU0) {
 		childregs->regs[28] = (unsigned long) p;
 		childregs->regs[29] = childksp;
