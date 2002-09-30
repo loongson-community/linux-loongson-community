61a542af7ef131f2c902ce6bf640054c6b2817d5
Ralf Baechle <ralf@linux-mips.org>
2002-09-30 17:00:06 +0000

    Missing declarations.

--- include/asm-mips64/page.h
+++ include/asm-mips64/page.h
@@ -25,10 +25,28 @@
 
 extern void (*_clear_page)(void * page);
 extern void (*_copy_page)(void * to, void * from);
+
 extern void mips64_clear_page_dc(unsigned long page);
 extern void mips64_clear_page_sc(unsigned long page);
+void r4k_clear_page_d16(void * page);
+void r4k_clear_page_d32(void * page);
+void r4k_clear_page_r4600_v1(void * page);
+void r4k_clear_page_r4600_v2(void * page);
+void r4k_clear_page_s16(void * page);
+void r4k_clear_page_s32(void * page);
+void r4k_clear_page_s64(void * page);
+void r4k_clear_page_s128(void * page);
+
 extern void mips64_copy_page_dc(unsigned long to, unsigned long from);
 extern void mips64_copy_page_sc(unsigned long to, unsigned long from);
+void r4k_copy_page_d16(void * to, void * from);
+void r4k_copy_page_d32(void * to, void * from);
+void r4k_copy_page_r4600_v1(void * to, void * from);
+void r4k_copy_page_r4600_v2(void * to, void * from);
+void r4k_copy_page_s16(void * to, void * from);
+void r4k_copy_page_s32(void * to, void * from);
+void r4k_copy_page_s64(void * to, void * from);
+void r4k_copy_page_s128(void * to, void * from);
 
 #define clear_page(page)	_clear_page(page)
 #define copy_page(to, from)	_copy_page(to, from)
