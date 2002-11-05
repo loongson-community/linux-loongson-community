d386674ad1162e9cd85192e0b65c1b9a2cfb8e68
Ralf Baechle <ralf@linux-mips.org>
2002-11-05 14:56:58 +0000

    De-fuckup new fp context switching code.

--- include/asm-mips/fpu.h
+++ include/asm-mips/fpu.h
@@ -2,18 +2,17 @@
  * Copyright (C) 2002 MontaVista Software Inc.
  * Author: Jun Sun, jsun@mvista.com or jsun@junsun.net
  *
- * This program is free software; you can redistribute  it and/or modify it
- * under  the terms of  the GNU General  Public License as published by the
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
  * Free Software Foundation;  either version 2 of the  License, or (at your
  * option) any later version.
- *
  */
-
 #ifndef _ASM_FPU_H
 #define _ASM_FPU_H
 
 #include <linux/config.h>
 #include <linux/sched.h>
+#include <linux/thread_info.h>
 
 #include <asm/mipsregs.h>
 #include <asm/cpu.h>
@@ -31,27 +30,27 @@ extern void _save_fp(struct task_struct *);
 extern void _restore_fp(struct task_struct *);
 
 #if defined(CONFIG_CPU_SB1)
-#define __enable_fpu_hazard()                                           \
-do {                                                                    \
-        asm(".set push          \n\t"                                   \
-            ".set mips64        \n\t"                                   \
-            ".set noreorder     \n\t"                                   \
-            "ssnop              \n\t"                                   \
-            "bnezl $0, .+4      \n\t"                                   \
-            "ssnop              \n\t"                                   \
-            ".set pop");                                                \
+#define __enable_fpu_hazard()						\
+do {									\
+	asm(".set	push		\n\t"				\
+	    ".set	mips2		# .set mips64	\n\t"		\
+	    ".set	noreorder	\n\t"				\
+	    "sll	$0, $0, 1	# ssnop		\n\t"		\
+	    "bnezl	$0, .+4		\n\t"				\
+	    "sll	$0, $0, 1	# ssnop		\n\t"		\
+	    ".set pop");						\
 } while (0)
 #else
-#define __enable_fpu_hazard()                                           \
-do {                                                                    \
-        asm("nop;nop;nop;nop");         /* max. hazard */               \
+#define __enable_fpu_hazard()						\
+do {									\
+	asm("nop;nop;nop;nop");		/* max. hazard */		\
 } while (0)
 #endif
 
-#define __enable_fpu()                                                  \
-do {                                                                    \
-        set_cp0_status(ST0_CU1);                                        \
-        __enable_fpu_hazard();                                          \
+#define __enable_fpu()							\
+do {									\
+	set_cp0_status(ST0_CU1);					\
+	__enable_fpu_hazard();						\
 } while (0)
 
 #define __disable_fpu()							\
@@ -73,20 +72,20 @@ do {									\
 } while (0)
 
 
-#define clear_fpu_owner() do {current->flags &= ~PF_USEDFPU; } while(0)
+#define clear_fpu_owner()	clear_thread_flag(TIF_USEDFPU)
 
 static inline int is_fpu_owner(void)
 {
 	return (mips_cpu.options & MIPS_CPU_FPU) && 
-		((current->flags & PF_USEDFPU) != 0); 
+		test_and_set_thread_flag(TIF_USEDFPU); 
 }
 
 static inline void own_fpu(void)
 {
-	if(mips_cpu.options & MIPS_CPU_FPU) {
+	if (mips_cpu.options & MIPS_CPU_FPU) {
 		__enable_fpu();
 		KSTK_STATUS(current) |= ST0_CU1;
-		current->flags |= PF_USEDFPU;
+		set_thread_flag(TIF_USEDFPU); 
 	}
 }
 
@@ -94,7 +93,7 @@ static inline void loose_fpu(void)
 {
 	if (mips_cpu.options & MIPS_CPU_FPU) {
 		KSTK_STATUS(current) &= ~ST0_CU1;
-		current->flags &= ~PF_USEDFPU;
+		clear_thread_flag(TIF_USEDFPU); 
 		__disable_fpu();
 	}
 }
@@ -122,7 +121,7 @@ static inline void restore_fp(struct task_struct *tsk)
 
 static inline unsigned long long *get_fpu_regs(struct task_struct *tsk)
 {
-	if(mips_cpu.options & MIPS_CPU_FPU) {
+	if (mips_cpu.options & MIPS_CPU_FPU) {
 		if ((tsk == current) && is_fpu_owner()) 
 			_save_fp(current);
 		return (unsigned long long *)&tsk->thread.fpu.hard.fp_regs[0];
@@ -132,4 +131,3 @@ static inline unsigned long long *get_fpu_regs(struct task_struct *tsk)
 }
 
 #endif /* _ASM_FPU_H */
-
