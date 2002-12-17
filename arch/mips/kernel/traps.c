25d0282ebe616bd54739236218a024fac124f36f
Jun Sun <jsun@junsun.net>
2002-12-17 01:49:11 +0000

    Typo in do_reserved().  Bit 5:6 are part of ExcCode.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -747,7 +747,7 @@ asmlinkage void do_reserved(struct pt_regs *regs)
 	 */
 	show_regs(regs);
 	panic("Caught reserved exception %ld - should not happen.",
-	      (regs->cp0_cause & 0x1f) >> 2);
+	      (regs->cp0_cause & 0x7f) >> 2);
 }
 
 static inline void watch_init(void)
