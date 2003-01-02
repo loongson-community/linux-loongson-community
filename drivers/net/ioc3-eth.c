355dd80f7ef02f5e5a8305123843c85836a9a7bf
Ralf Baechle <ralf@linux-mips.org>
2003-01-02 13:58:39 +0000

    Fix printing of MAC address.  Fix getting the MAC address which was always being read out as zero.  Seems somebody at SGI needs to read his patches before sending them to Linus ...

--- drivers/net/ioc3-eth.c
+++ drivers/net/ioc3-eth.c
@@ -460,30 +460,25 @@ static void ioc3_get_eaddr_sn(struct ioc3_private *ip)
  */
 static void ioc3_get_eaddr(struct ioc3_private *ip)
 {
-	void (*do_get_eaddr)(struct ioc3_private *ip) = NULL;
 	int i;
 
+
+#ifdef CONFIG_SGI_IP27
+	ioc3_get_eaddr_nic(ip);
+#elif defined(CONFIG_IA64_SGI_SN1) || defined(CONFIG_IA64_SGI_SN2)
 	/*
 	 * We should also use this code for PCI cards, no matter what host
 	 * machine but how to know that we're a PCI card?
 	 */
-#ifdef CONFIG_SGI_IP27
-	do_get_eaddr = ioc3_get_eaddr_nic;
+	ioc3_get_eaddr_sn(ip);
+#else 
+#error No clue how to read IOC3 MAC address for this configuration.
 #endif
-#if defined(CONFIG_IA64_SGI_SN1) || defined(CONFIG_IA64_SGI_SN2)
-	do_get_eaddr = ioc3_get_eaddr_sn;
-#endif
-
-	if (!do_get_eaddr) {
-		printk(KERN_ERR "Don't know how to read MAC address of this "
-		       "IOC3 NIC\n");
-		return;
-	}
 
 	printk("Ethernet address is ");
 	for (i = 0; i < 6; i++) {
 		printk("%02x", ip->dev->dev_addr[i]);
-		if (i < 7)
+		if (i < 5)
 			printk(":");
 	}
 	printk(".\n");
