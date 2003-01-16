941a95bae752293044ad5db642ddb3e150de722a
Maciej W. Rozycki <macro@linux-mips.org>
2003-01-16 13:53:36 +0000

    Kill a warning.  Optimize a bit.

--- arch/mips/dec/reset.c
+++ arch/mips/dec/reset.c
@@ -2,12 +2,13 @@
  * Reset a DECstation machine.
  *
  * Copyright (C) 199x  the Anonymous
- * Copyright (C) 2001, 2002  Maciej W. Rozycki
+ * Copyright (C) 2001, 2002, 2003  Maciej W. Rozycki
  */
 
 #include <asm/addrspace.h>
+#include <asm/ptrace.h>
 
-void (*back_to_prom)(void) = (void (*)(void))KSEG1ADDR(0x1fc00000);
+#define back_to_prom()	(((void (*)(void))KSEG1ADDR(0x1fc00000))())
 
 void dec_machine_restart(char *command)
 {
