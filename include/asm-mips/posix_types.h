af25ffd60bad9c77bfe106a0f7eb723e8781b399
Ralf Baechle <ralf@linux-mips.org>
2003-01-20 12:17:19 +0000

    Make nlink_t unsigned long.

--- include/asm-mips/posix_types.h
+++ include/asm-mips/posix_types.h
@@ -17,7 +17,7 @@
 typedef unsigned int	__kernel_dev_t;
 typedef unsigned long	__kernel_ino_t;
 typedef unsigned int	__kernel_mode_t;
-typedef int		__kernel_nlink_t;
+typedef unsigned long	__kernel_nlink_t;
 typedef long		__kernel_off_t;
 typedef int		__kernel_pid_t;
 typedef int		__kernel_ipc_pid_t;
