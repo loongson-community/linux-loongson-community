4d8e1237cd2d8e2218fb8b2ed649219ef60a0e8a
Ralf Baechle <ralf@linux-mips.org>
2003-01-27 19:19:44 +0000

    More type fixes.

--- include/asm-mips64/signal.h
+++ include/asm-mips64/signal.h
@@ -16,7 +16,7 @@
 #define _NSIG_WORDS	(_NSIG / _NSIG_BPW)
 
 typedef struct {
-	long sig[_NSIG_WORDS];
+	unsigned long sig[_NSIG_WORDS];
 } sigset_t;
 
 #define _NSIG32		128
@@ -24,7 +24,7 @@ typedef struct {
 #define _NSIG_WORDS32	(_NSIG32 / _NSIG_BPW32)
 
 typedef struct {
-	long sig[_NSIG_WORDS32];
+	unsigned int sig[_NSIG_WORDS32];
 } sigset_t32;
 
 typedef unsigned long old_sigset_t;		/* at least 32 bits */
