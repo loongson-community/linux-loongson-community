0d244d0c2b8635d5a8deb70c834ebb65b82ec636
Ralf Baechle <ralf@linux-mips.org>
2003-02-05 21:22:23 +0000

    Fix parsing of MAC address passed on command line.

--- arch/mips/au1000/common/prom.c
+++ arch/mips/au1000/common/prom.c
@@ -105,9 +105,11 @@ char *prom_getenv(char *envname)
 inline unsigned char str2hexnum(unsigned char c)
 {
 	if(c >= '0' && c <= '9')
-	return c - '0';
+		return c - '0';
 	if(c >= 'a' && c <= 'f')
-	return c - 'a' + 10;
+		return c - 'a' + 10;
+	if(c >= 'A' && c <= 'F')
+		return c - 'A' + 10;
 	return 0; /* foo */
 }
 
