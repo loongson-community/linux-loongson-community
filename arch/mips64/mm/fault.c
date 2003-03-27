990304d99268dac7c2fc2cf24e5039870d69d900
Ralf Baechle <ralf@linux-mips.org>
2003-03-27 12:23:03 +0000

    Zap debug crap that should have made it into CVS.

--- arch/mips64/mm/fault.c
+++ arch/mips64/mm/fault.c
@@ -188,7 +188,6 @@ bad_area:
 		       address,
 		       (unsigned long) regs->cp0_epc,
 		       (unsigned long) regs->regs[31]);
-while(1);
 #endif
 		info.si_signo = SIGSEGV;
 		info.si_errno = 0;
