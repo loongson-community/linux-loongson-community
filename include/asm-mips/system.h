3c1377fbda33fade560eb2f1d4d28bd920df4b45
Ralf Baechle <ralf@linux-mips.org>
2003-05-27 05:22:51 +0000

    Remove unneeded #ifdef __ASSEMBLY__ wrapper.

--- include/asm-mips/system.h
+++ include/asm-mips/system.h
@@ -271,13 +271,11 @@ do { var = value; mb(); } while (0)
 #define set_wmb(var, value) \
 do { var = value; wmb(); } while (0)
 
-#ifndef __ASSEMBLY__
 /*
  * switch_to(n) should switch tasks to task nr n, first
  * checking that n isn't the current task, in which case it does nothing.
  */
 extern asmlinkage void *resume(void *last, void *next, void *next_ti);
-#endif /* !__ASSEMBLY__ */
 
 struct task_struct;
 
