f497a6d66cd9fabb0e5bce956378e85052e9cf3c
Ralf Baechle <ralf@linux-mips.org>
2003-07-06 21:28:05 +0000

    Add back R4300.

--- arch/mips/mm/c-r4k.c
+++ arch/mips/mm/c-r4k.c
@@ -724,6 +724,7 @@ static void __init probe_pcache(void)
 	case CPU_R4400PC:
 	case CPU_R4400SC:
 	case CPU_R4400MC:
+	case CPU_R4300:
 		icache_size = 1 << (12 + ((config & CONF_IC) >> 9));
 		c->icache.linesz = 16 << ((config & CONF_IB) >> 5);
 		c->icache.ways = 1;
