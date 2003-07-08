e824c0362eaaa66d8c62816f88f7d10314f024eb
Ladislav Michl <ladis@linux-mips.org>
2003-07-08 20:16:38 +0000

    don't initialize twice, minor cleanup.

--- drivers/video/console/newport_con.c
+++ drivers/video/console/newport_con.c
@@ -52,6 +52,8 @@ static int xcurs_correction = 29;
 static int newport_xsize;
 static int newport_ysize;
 
+static int newport_set_def_font(int unit, struct console_font_op *op);
+
 #define BMASK(c) (c << 24)
 
 #define RENDER(regs, cp) do { \
@@ -281,23 +283,22 @@ static void newport_get_revisions(void)
 		xcurs_correction = 21;
 }
 
-static const char *__init newport_startup(void)
+/* Can't be __init, take_over_console may call it later */
+static const char *newport_startup(void)
 {
 	int i;
-	struct newport_regs *p;
 
 	if (!sgi_gfxaddr)
 		return NULL;
 	npregs = (struct newport_regs *) (KSEG1 + sgi_gfxaddr);
-	p = npregs;
-	p->cset.config = NPORT_CFG_GD0;
+	npregs->cset.config = NPORT_CFG_GD0;
 
 	if (newport_wait()) {
 		return NULL;
 	}
 
-	p->set.xstarti = TESTVAL;
-	if (p->set._xstart.word != XSTI_TO_FXSTART(TESTVAL))
+	npregs->set.xstarti = TESTVAL;
+	if (npregs->set._xstart.word != XSTI_TO_FXSTART(TESTVAL))
 		return NULL;
 
 	for (i = 0; i < MAX_NR_CONSOLES; i++)
@@ -307,8 +308,6 @@ static const char *__init newport_startup(void)
 	newport_get_revisions();
 	newport_get_screensize();
 
-	/* gfx_init (display_desc); */
-
 	return "SGI Newport";
 }
 
@@ -317,6 +316,15 @@ static void newport_init(struct vc_data *vc, int init)
 	vc->vc_cols = newport_xsize / 8;
 	vc->vc_rows = newport_ysize / 16;
 	vc->vc_can_do_color = 1;
+}
+
+static void newport_deinit(struct vc_data *c)
+{
+	int i;
+
+	/* free memory used by user font */
+	for (i = 0; i < MAX_NR_CONSOLES; i++)
+		newport_set_def_font(i, NULL);
 }
 
 static void newport_clear(struct vc_data *vc, int sy, int sx, int height,
@@ -699,7 +707,7 @@ static int newport_dummy(struct vc_data *c)
 const struct consw newport_con = {
     .con_startup =	newport_startup,
     .con_init =		newport_init,
-    .con_deinit =	DUMMY,
+    .con_deinit =	newport_deinit,
     .con_clear =	newport_clear,
     .con_putc =		newport_putc,
     .con_putcs =	newport_putcs,
@@ -715,14 +723,9 @@ const struct consw newport_con = {
     .con_save_screen =	DUMMY
 };
 
-MODULE_LICENSE("GPL");
-
+#ifdef MODULE
 static int __init newport_console_init(void)
 {
-	if (!newport_startup())
-		printk("Error loading SGI Newport Console driver\n");
-	else
-		printk("Loading SGI Newport Console Driver\n");
 	take_over_console(&newport_con, 0, MAX_NR_CONSOLES - 1, 1);
 
 	return 0;
@@ -730,14 +733,11 @@ static int __init newport_console_init(void)
 
 static void __exit newport_console_exit(void)
 {
-	int i;
-
-	printk("Unloading SGI Newport Console Driver\n");
-
-	/* free memory used by user font */
-	for (i = 0; i < MAX_NR_CONSOLES; i++)
-		newport_set_def_font(i, NULL);
+	give_up_console(&newport_con);
 }
 
 module_init(newport_console_init);
 module_exit(newport_console_exit);
+#endif
+
+MODULE_LICENSE("GPL");
