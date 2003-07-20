234a2eb243be9970b37d0ff8daf9ef196dd69daa
Ralf Baechle <ralf@linux-mips.org>
2003-07-20 21:38:33 +0000

    Temporarily glue CLOCK_TICK_RATE for a few systems.

--- include/asm-mips/timex.h
+++ include/asm-mips/timex.h
@@ -8,7 +8,33 @@
 #ifndef _ASM_TIMEX_H
 #define _ASM_TIMEX_H
 
+#include <linux/config.h>
 #include <asm/mipsregs.h>
+
+/*
+ * This is the frequency of the timer used for Linux's timer interrupt.
+ * The value should be defined as accurate as possible or under certain
+ * circumstances Linux timekeeping might become inaccurate or fail.
+ *
+ * For IP22 we cheat and pretend to have a 1MHz timer whic isn't strictly
+ * true - we only use the 8259 timer to calibrate the actual interrupt
+ * timer, so after all it's the master clock source of the system.
+ *
+ * The obscure number 1193182 is the same as used by the original i8254
+ * time in legacy PC hardware; the chip unfortunately also found in a
+ * bunch of MIPS systems.
+ */
+#ifdef CONFIG_ACER_PICA_61
+#define CLOCK_TICK_RATE		1193182
+#elif defined(CONFIG_MIPS_MAGNUM_4000)
+#define CLOCK_TICK_RATE		1193182
+#elif defined(CONFIG_OLIVETTI_M700)
+#define CLOCK_TICK_RATE		1193182
+#elif defined(CONFIG_SGI_IP22)
+#define CLOCK_TICK_RATE		1000000
+#elif defined(CONFIG_SNI_RM200_PCI)
+#define CLOCK_TICK_RATE		1193182
+#endif
 
 /*
  * Standard way to access the cycle counter.
