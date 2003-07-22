64a78dd8f45d4260c9cdd1fdd2cad2c4264404a4
Ralf Baechle <ralf@linux-mips.org>
2003-07-22 02:13:49 +0000

    Doing the address arithmetic in C both files become identical ...

--- arch/mips/mm/pg-sb1.c
+++ arch/mips/mm/pg-sb1.c
@@ -21,7 +21,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 #include <linux/config.h>
 #include <linux/sched.h>
 #include <linux/smp.h>
@@ -42,6 +41,9 @@
 /* These are the functions hooked by the memory management function pointers */
 void sb1_clear_page(void *page)
 {
+	unsigned char *addr = (unsigned char *) page;
+	unsigned char *end = addr + PAGE_SIZE;
+
 	/*
 	 * JDCXXX - This should be bottlenecked by the write buffer, but these
 	 * things tend to be mildly unpredictable...should check this on the
@@ -51,36 +53,36 @@ void sb1_clear_page(void *page)
 	 * since we know we're on an SB1, we force the assembler to take
 	 * 64-bit operands to speed things up
 	 */
-	__asm__ __volatile__(
-		".set push                  \n"
-		".set noreorder             \n"
-		".set noat                  \n"
-		".set mips4                 \n"
-		"     daddiu     $1, %0, %2  \n"  /* Calculate the end of the page to clear */
+	do {
+		__asm__ __volatile__(
+		"	.set	mips4		\n"
 #ifdef CONFIG_CPU_HAS_PREFETCH
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ",  0(%0)  \n"  /* Prefetch the first 4 lines */
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ", 32(%0)  \n"
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ", 64(%0)  \n"
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ", 96(%0)  \n"
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ",  0(%0)  \n"  /* Prefetch the first 4 lines */
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ", 32(%0)  \n"
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ", 64(%0)  \n"
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ", 96(%0)  \n"
 #endif
-		"1:   sd        $0,  0(%0)  \n"  /* Throw out a cacheline of 0's */
-		"     sd        $0,  8(%0)  \n"
-		"     sd        $0, 16(%0)  \n"
-		"     sd        $0, 24(%0)  \n"
+		"1:	sd	$0,  0(%0)	\n"  /* Throw out a cacheline of 0's */
+		"	sd	$0,  8(%0)	\n"
+		"	sd	$0, 16(%0)	\n"
+		"	sd	$0, 24(%0)	\n"
 #ifdef CONFIG_CPU_HAS_PREFETCH
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ",128(%0)  \n"  /* Prefetch 4 lines ahead     */
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ",128(%0)  \n"  /* Prefetch 4 lines ahead     */
 #endif
-		"     bne       $1, %0, 1b  \n"
-		"      daddiu     %0, %0, 32\n"  /* Next cacheline (This instruction better be short piped!) */
-		".set pop                   \n"
-		: "=r" (page)
-		: "0" (page), "I" (PAGE_SIZE-32)
+		"	.set	mips0		\n"
+		:
+		: "r" (addr)
 		: "memory");
-
+		addr += 32;
+	} while (addr != end);
 }
 
 void sb1_copy_page(void *to, void *from)
 {
+	unsigned char *src = from;
+	unsigned char *dst = to;
+	unsigned char *end = src + PAGE_SIZE;
+
 	/*
 	 * This should be optimized in assembly...can't use ld/sd, though,
 	 * because the top 32 bits could be nuked if we took an interrupt
@@ -93,49 +95,44 @@ void sb1_copy_page(void *to, void *from)
 	 * performance data to back this up
 	 */
 
-	__asm__ __volatile__(
-		".set push                  \n"
-		".set noreorder             \n"
-		".set noat                  \n"
-		".set mips4                 \n"
-		"     daddiu     $1, %0, %4  \n"  /* Calculate the end of the page to copy */
+	do {
+		__asm__ __volatile__(
+		"	.set	mips4					\n"
 #ifdef CONFIG_CPU_HAS_PREFETCH
-		"     pref       " SB1_PREF_LOAD_STREAMED_HINT  ",  0(%0)  \n"  /* Prefetch the first 3 lines */
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ",  0(%1)  \n"
-		"     pref       " SB1_PREF_LOAD_STREAMED_HINT  ",  32(%0) \n"
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ",  32(%1) \n"
-		"     pref       " SB1_PREF_LOAD_STREAMED_HINT  ",  64(%0) \n"
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ",  64(%1) \n"
+		"	pref	" SB1_PREF_LOAD_STREAMED_HINT  ",  0(%0)\n"  /* Prefetch the first 3 lines */
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ",  0(%1)\n"
+		"	pref	" SB1_PREF_LOAD_STREAMED_HINT  ",  32(%0)\n"
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ",  32(%1)\n"
+		"	pref	" SB1_PREF_LOAD_STREAMED_HINT  ",  64(%0)\n"
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ",  64(%1)\n"
 #endif
-		"1:   lw        $2,  0(%0)  \n"  /* Block copy a cacheline */
-		"     lw        $3,  4(%0)  \n"
-		"     lw        $4,  8(%0)  \n"
-		"     lw        $5, 12(%0)  \n"
-		"     lw        $6, 16(%0)  \n"
-		"     lw        $7, 20(%0)  \n"
-		"     lw        $8, 24(%0)  \n"
-		"     lw        $9, 28(%0)  \n"
+		"1:	lw	$2,  0(%0)	\n"  /* Block copy a cacheline */
+		"	lw	$3,  4(%0)	\n"
+		"	lw	$4,  8(%0)	\n"
+		"	lw	$5, 12(%0)	\n"
+		"	lw	$6, 16(%0)	\n"
+		"	lw	$7, 20(%0)	\n"
+		"	lw	$8, 24(%0)	\n"
+		"	lw	$9, 28(%0)	\n"
 #ifdef CONFIG_CPU_HAS_PREFETCH
-		"     pref       " SB1_PREF_LOAD_STREAMED_HINT  ", 96(%0)  \n"  /* Prefetch ahead         */
-		"     pref       " SB1_PREF_STORE_STREAMED_HINT ", 96(%1)  \n"
+		"	pref	" SB1_PREF_LOAD_STREAMED_HINT  ", 96(%0)  \n"  /* Prefetch ahead         */
+		"	pref	" SB1_PREF_STORE_STREAMED_HINT ", 96(%1)  \n"
 #endif
-		"     sw        $2,  0(%1)  \n"
-		"     sw        $3,  4(%1)  \n"
-		"     sw        $4,  8(%1)  \n"
-		"     sw        $5, 12(%1)  \n"
-		"     sw        $6, 16(%1)  \n"
-		"     sw        $7, 20(%1)  \n"
-		"     sw        $8, 24(%1)  \n"
-		"     sw        $9, 28(%1)  \n"
-		"     daddiu     %1, %1, 32  \n"  /* Next cacheline */
-		"     nop                   \n"  /* Force next add to short pipe */
-		"     nop                   \n"  /* Force next add to short pipe */
-		"     bne       $1, %0, 1b  \n"
-		"     daddiu     %0, %0, 32  \n"  /* Next cacheline */
-		".set pop                   \n"
-		: "=r" (to), "=r" (from)
-		: "0" (from), "1" (to), "I" (PAGE_SIZE-32)
+		"	sw	$2,  0(%1)	\n"
+		"	sw	$3,  4(%1)	\n"
+		"	sw	$4,  8(%1)	\n"
+		"	sw	$5, 12(%1)	\n"
+		"	sw	$6, 16(%1)	\n"
+		"	sw	$7, 20(%1)	\n"
+		"	sw	$8, 24(%1)	\n"
+		"	sw	$9, 28(%1)	\n"
+		"	.set	mips0		\n"
+		:
+		: "r" (src), "r" (dst)
 		: "$2","$3","$4","$5","$6","$7","$8","$9","memory");
+		src += 32;
+		dst += 32;
+	} while (src != end);
 }
 
 
