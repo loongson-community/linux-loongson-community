a5f49ce159adf506bef99a9b247163eadea696cd
Kip Walker <kwalker@broadcom.com>
2003-08-20 16:37:26 +0000

    must define L1_CACHE_SHIFT (fs/dquot.c needs it)

--- include/asm-mips/cache.h
+++ include/asm-mips/cache.h
@@ -14,12 +14,13 @@
 #if defined(CONFIG_CPU_R3000) || defined(CONFIG_CPU_R6000) || \
     defined(CONFIG_CPU_TX39XX)
 #define L1_CACHE_BYTES		16
-#define L1_CACHE_SHIFT_MAX	 4	/* largest L1 which this arch supports */
+#define L1_CACHE_SHIFT		 4	/* largest L1 which this arch supports */
 #else
 #define L1_CACHE_BYTES 		32	/* A guess */
-#define L1_CACHE_SHIFT_MAX	 6	/* largest L1 which this arch supports */
+#define L1_CACHE_SHIFT		 6	/* largest L1 which this arch supports */
 #endif
 
+#define L1_CACHE_SHIFT_MAX	L1_CACHE_SHIFT
 #define SMP_CACHE_BYTES		L1_CACHE_BYTES
 
 #endif /* _ASM_CACHE_H */
