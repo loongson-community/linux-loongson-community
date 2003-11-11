b2e81dd8ce6054509c767028963953730398c79f
Ralf Baechle <ralf@linux-mips.org>
2003-11-11 12:36:07 +0000

    And yet another pcibios_fixup_resources ...

--- arch/mips/au1000/common/pci_fixup.c
+++ arch/mips/au1000/common/pci_fixup.c
@@ -53,11 +53,6 @@ static void fixup_resource(int r_num, struct pci_dev *dev) ;
 static unsigned long virt_io_addr;
 #endif
 
-void __init pcibios_fixup_resources(struct pci_dev *dev)
-{
-	/* will need to fixup IO resources */
-}
-
 /*
  * Ralf: This isn't a fixup but yet necessary, so I didn't delete the code.
  * Anyway, pcibios_fixup is NOT called anymore so this means Au1x00 PCI
