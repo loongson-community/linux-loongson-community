72913a3e3601b3210b51f1c03f2acdb15c28596f
Ralf Baechle <ralf@linux-mips.org>
2003-11-15 03:12:00 +0000

    Duh...  Correct code but the function names were swapped.

--- arch/mips/mm/dma-noncoherent.c
+++ arch/mips/mm/dma-noncoherent.c
@@ -59,15 +59,15 @@ void *dma_alloc_coherent(struct device *dev, size_t size,
 
 EXPORT_SYMBOL(dma_alloc_coherent);
 
-void dma_free_coherent(struct device *dev, size_t size, void *vaddr,
+void dma_free_noncoherent(struct device *dev, size_t size, void *vaddr,
 	dma_addr_t dma_handle)
 {
 	free_pages((unsigned long) vaddr, get_order(size));
 }
 
-EXPORT_SYMBOL(dma_free_coherent);
+EXPORT_SYMBOL(dma_free_noncoherent);
 
-void dma_free_noncoherent(struct device *dev, size_t size, void *vaddr,
+void dma_free_coherent(struct device *dev, size_t size, void *vaddr,
 	dma_addr_t dma_handle)
 {
 	unsigned long addr = (unsigned long) vaddr;
@@ -76,7 +76,7 @@ void dma_free_noncoherent(struct device *dev, size_t size, void *vaddr,
 	free_pages(addr, get_order(size));
 }
 
-EXPORT_SYMBOL(dma_free_noncoherent);
+EXPORT_SYMBOL(dma_free_coherent);
 
 static inline void __dma_sync(unsigned long addr, size_t size,
 	enum dma_data_direction direction)
