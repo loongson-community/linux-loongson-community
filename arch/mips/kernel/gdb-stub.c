d85175bb15e086c84d51525a6c01b7a6c54397f4
Ralf Baechle <ralf@linux-mips.org>
2004-01-19 18:57:39 +0000

    Remove unnecessary \t's in inline assembler.

--- arch/mips/kernel/gdb-stub.c
+++ arch/mips/kernel/gdb-stub.c
@@ -986,7 +986,7 @@ void breakpoint(void)
 	__asm__ __volatile__(
 			".globl	breakinst\n\t" 
 			".set\tnoreorder\n\t"
-			"nop\n\t"
+			"nop\n\
 			"breakinst:\tbreak\n\t"
 			"nop\n\t"
 			".set\treorder"
@@ -999,7 +999,7 @@ void async_breakpoint(void)
 	__asm__ __volatile__(
 			".globl	async_breakinst\n\t" 
 			".set\tnoreorder\n\t"
-			"nop\n\t"
+			"nop\n"
 			"async_breakinst:\tbreak\n\t"
 			"nop\n\t"
 			".set\treorder"
