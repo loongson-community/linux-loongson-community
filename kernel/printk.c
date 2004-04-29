34b539a3efe21e40a152f4b6bb3f5b3d1111f9ba
Ralf Baechle <ralf@linux-mips.org>
2004-04-29 15:53:38 +0000

    Make console_setup static again - as it should be ...

--- kernel/printk.c
+++ kernel/printk.c
@@ -116,7 +116,7 @@ static int console_may_schedule;
 /*
  *	Setup a list of consoles. Called from init/main.c
  */
-int __init console_setup(char *str)
+static int __init console_setup(char *str)
 {
 	char name[sizeof(console_cmdline[0].name)];
 	char *s, *options;
