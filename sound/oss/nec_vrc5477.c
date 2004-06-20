7f0f59d66cca2796d8816c5d92e760ccf7448d9c
Ralf Baechle <ralf@linux-mips.org>
2004-06-20 00:18:39 +0000

    Remove duplicated (C) notice.

--- sound/oss/nec_vrc5477.c
+++ sound/oss/nec_vrc5477.c
@@ -8,8 +8,6 @@
  *
  * VRA support Copyright 2001 Bradley D. LaRonde <brad@ltc.com>
  *
- * VRA support Copyright 2001 Bradley D. LaRonde <brad@ltc.com>
- *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
  * Free Software Foundation;  either version 2 of the  License, or (at your
