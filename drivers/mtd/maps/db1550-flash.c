af010ffe9225ec809758c6e700896125f6a85c33
Pete Popov <ppopov@embeddedalley.com>
2004-10-12 06:01:04 +0000

    Synced mtd driver with latest mtd head changes.

--- drivers/mtd/maps/db1550-flash.c
+++ drivers/mtd/maps/db1550-flash.c
@@ -1,10 +1,10 @@
 /*
  * Flash memory access on Alchemy Db1550 board
  * 
- * $Id: db1550-flash.c,v 1.3 2004/07/14 17:45:40 dwmw2 Exp $
+ * $Id: db1550-flash.c,v 1.5 2004/09/19 00:12:00 ppopov Exp $
  *
  * (C) 2004 Embedded Edge, LLC, based on db1550-flash.c:
- * (C) 2003 Pete Popov <pete_popov@yahoo.com>
+ * (C) 2003, 2004 Pete Popov <ppopov@embeddedalley.com>
  * 
  */
 
@@ -19,7 +19,6 @@
 #include <linux/mtd/partitions.h>
 
 #include <asm/io.h>
-#include <asm/au1000.h>
 
 #ifdef 	DEBUG_RW
 #define	DBG(x...)	printk(x)
@@ -163,7 +162,7 @@ int __init db1550_mtd_init(void)
 	printk(KERN_NOTICE "Pb1550 flash: probing %d-bit flash bus\n", 
 			db1550_map.bankwidth*8);
 	db1550_map.virt = 
-		(unsigned long)ioremap(window_addr, window_size);
+		(void __iomem *)ioremap(window_addr, window_size);
 	mymtd = do_map_probe("cfi_probe", &db1550_map);
 	if (!mymtd) return -ENXIO;
 	mymtd->owner = THIS_MODULE;
