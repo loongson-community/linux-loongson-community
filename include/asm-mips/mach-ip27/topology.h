e2718c94a96a95e82bc1705f69cc1ab599990989
Ralf Baechle <ralf@linux-mips.org>
2004-11-14 03:15:47 +0000

    Provide SD_NODE_INIT definition to get CONFIG_NUMA to work.

--- include/asm-mips/mach-ip27/topology.h
+++ include/asm-mips/mach-ip27/topology.h
@@ -12,4 +12,24 @@
 extern int node_distance(nasid_t nasid_a, nasid_t nasid_b);
 #define node_distance(from, to)	node_distance(from, to)
 
+/* sched_domains SD_NODE_INIT for SGI IP27 machines */
+#define SD_NODE_INIT (struct sched_domain) {		\
+	.span			= CPU_MASK_NONE,	\
+	.parent			= NULL,			\
+	.groups			= NULL,			\
+	.min_interval		= 8,			\
+	.max_interval		= 32,			\
+	.busy_factor		= 32,			\
+	.imbalance_pct		= 125,			\
+	.cache_hot_time		= (10*1000),		\
+	.cache_nice_tries	= 1,			\
+	.per_cpu_gain		= 100,			\
+	.flags			= SD_LOAD_BALANCE	\
+				| SD_BALANCE_EXEC	\
+				| SD_WAKE_BALANCE,	\
+	.last_balance		= jiffies,		\
+	.balance_interval	= 1,			\
+	.nr_balance_failed	= 0,			\
+}
+
 #endif /* _ASM_MACH_TOPOLOGY_H */
