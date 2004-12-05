28f054dd3c765bebb2d7c35d5823327005b2a3c8
Ralf Baechle <ralf@linux-mips.org>
2004-12-05 12:17:42 +0000

    Use the c0_count / c0_compare as interrupt timer because we don't seem to have anything better.

--- arch/mips/pmc-sierra/yosemite/smp.c
+++ arch/mips/pmc-sierra/yosemite/smp.c
@@ -4,6 +4,9 @@
 #include <asm/pmon.h>
 #include <asm/titan_dep.h>
 
+extern unsigned int (*mips_hpt_read)(void);
+extern void (*mips_hpt_init)(unsigned int);
+
 #define LAUNCHSTACK_SIZE 256
 
 static spinlock_t launch_lock __initdata;
@@ -103,6 +106,8 @@ void prom_cpus_done(void)
  */
 void prom_init_secondary(void)
 {
+	mips_hpt_init(mips_hpt_read());
+
 	set_c0_status(ST0_CO | ST0_IE | ST0_IM);
 }
 
