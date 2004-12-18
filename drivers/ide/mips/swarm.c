74c516818f992e968216fa2764ef94dc2338cd42
Maciej W. Rozycki <macro@linux-mips.org>
2004-12-18 02:06:38 +0000

    Use mmio operations correctly as the interface is not on PCI. Reserve the iomem resource used.  Remove private I/O accessory functions as no longer needed.

--- drivers/ide/mips/swarm.c
+++ drivers/ide/mips/swarm.c
@@ -1,5 +1,9 @@
 /*
  * Copyright (C) 2001, 2002, 2003 Broadcom Corporation
+ * Copyright (C) 2004 MontaVista Software Inc.
+ *	Author:	Manish Lachwani, mlachwani@mvista.com
+ * Copyright (C) 2004  MIPS Technologies, Inc.  All rights reserved.
+ *	Author: Maciej W. Rozycki <macro@mips.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,13 +20,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-/*  Derived loosely from ide-pmac.c, so:
- *  
+/*
+ *  Derived loosely from ide-pmac.c, so:  
  *  Copyright (C) 1998 Paul Mackerras.
  *  Copyright (C) 1995-1998 Mark Lord
- *
- *  Copyright (C) 2004 MontaVista Software Inc.
- *  Author: Manish Lachwani, mlachwani@mvista.com
  */
 
 /*
@@ -32,107 +33,27 @@
  * other PCI devices, for example, will require swapping).  Any
  * SiByte-targetted kernel including IDE support will include this
  * file.  Probing of a Generic Bus for an IDE device is controlled by
- * the definitions of "SIBYTE_HAVE_IDE" and "IDE_PHYS", which are
- * provided by <asm/sibyte/board.h> for Broadcom boards.
+ * the definition of "SIBYTE_HAVE_IDE", which is provided by
+ * <asm/sibyte/board.h> for Broadcom boards.
  */
 
-#include <linux/kernel.h>
 #include <linux/ide.h>
+#include <linux/ioport.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+
+#include <asm/io.h>
+
 #include <asm/sibyte/board.h>
+#include <asm/sibyte/sb1250_genbus.h>
+#include <asm/sibyte/sb1250_regs.h>
 
-/*
- * Our non-swapping I/O operations.  
- */
+#define DRV_NAME "ide-swarm"
 
-static inline void sibyte_outb(u8 val, unsigned long port) {
-	*(volatile u8 *)(mips_io_port_base + (port)) = val;
-}
-
-static inline void sibyte_outw(u16 val, unsigned long port) {
-	*(volatile u16 *)(mips_io_port_base + (port)) = val;
-}
-
-static inline void sibyte_outl(u32 val, unsigned long port) {
-	*(volatile u32 *)(mips_io_port_base + (port)) = val;
-}
-
-static inline unsigned char sibyte_inb(unsigned long port)
-{
-	return (*(volatile u8 *)(mips_io_port_base + (port)));
-}
-
-static inline unsigned short sibyte_inw(unsigned long port)
-{
-	return (*(volatile u16 *)(mips_io_port_base + (port)));
-}
-
-static inline unsigned int sibyte_inl(unsigned long port)
-{
-	return (*(volatile u32 *)(mips_io_port_base + (port)));
-}
-
-
-static inline void sibyte_outsb(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		sibyte_outb(*(u8 *)addr, port);
-		addr++;
-	}
-}
-
-static inline void sibyte_insb(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		*(u8 *)addr = sibyte_inb(port);
-		addr++;
-	}
-}
-
-static inline void sibyte_outsw(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		sibyte_outw(*(u16 *)addr, port);
-		addr += 2;
-	}
-}
-
-static inline void sibyte_insw(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		*(u16 *)addr = sibyte_inw(port);
-		addr += 2;
-	}
-}
-
-static inline void sibyte_outsl(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		sibyte_outl(*(u32 *)addr, port);
-		addr += 4;
-	}
-}
-
-static inline void sibyte_insl(unsigned long port, void *addr, unsigned int count)
-{
-	while (count--) {
-		*(u32 *)addr = sibyte_inl(port);
-		addr += 4;
-	}
-}
-
-static void sibyte_set_ideops(ide_hwif_t *hwif)
-{
-	hwif->INB = sibyte_inb;
-	hwif->INW = sibyte_inw;
-	hwif->INL = sibyte_inl;
-	hwif->OUTB = sibyte_outb;
-	hwif->OUTW = sibyte_outw;
-	hwif->OUTL = sibyte_outl;
-	hwif->INSW = sibyte_insw;
-	hwif->INSL = sibyte_insl;
-	hwif->OUTSW = sibyte_outsw;
-	hwif->OUTSL = sibyte_outsl;
-}
+static struct resource swarm_ide_resource = {
+	.name	= "SWARM GenBus IDE",
+	.flags	= IORESOURCE_MEM,
+};
 
 /*
  * swarm_ide_probe - if the board header indicates the existence of
@@ -140,49 +61,63 @@ static void sibyte_set_ideops(ide_hwif_t *hwif)
  */
 void __init swarm_ide_probe(void)
 {
-#if defined(SIBYTE_HAVE_IDE) && defined(IDE_PHYS)
-	int i = 0;
-	ide_hwif_t *sb_ide_hwif;
+	ide_hwif_t *hwif;
+	u8 __iomem *base;
+	phys_t offset, size;
+	int i;
 
+	if (!SIBYTE_HAVE_IDE)
+		return;
+
+	/* Find an empty slot.  */
 	for (i = 0; i < MAX_HWIFS; i++) 
-		if (!ide_hwifs[i].io_ports[IDE_DATA_OFFSET]) {
-			/* Find an empty slot */
+		if (!ide_hwifs[i].io_ports[IDE_DATA_OFFSET])
 			break;
-		}
+	if (i >= MAX_HWIFS) {
+		printk(KERN_ERR DRV_NAME ": no free slot for interface\n");
+		return;
+	}
+	hwif = ide_hwifs + i;
 
-	/*
-	 * Preadjust for mips_io_port_base since the I/O ops expect
-	 * relative addresses
-	 */
-#define SIBYTE_IDE_REG(pcaddr) (IOADDR(IDE_PHYS) + ((pcaddr)<<5) - mips_io_port_base)
-
-	sb_ide_hwif = &ide_hwifs[i];
-
-	sb_ide_hwif->hw.io_ports[IDE_DATA_OFFSET]    = SIBYTE_IDE_REG(0x1f0);
-	sb_ide_hwif->hw.io_ports[IDE_ERROR_OFFSET]   = SIBYTE_IDE_REG(0x1f1);
-	sb_ide_hwif->hw.io_ports[IDE_NSECTOR_OFFSET] = SIBYTE_IDE_REG(0x1f2);
-	sb_ide_hwif->hw.io_ports[IDE_SECTOR_OFFSET]  = SIBYTE_IDE_REG(0x1f3);
-	sb_ide_hwif->hw.io_ports[IDE_LCYL_OFFSET]    = SIBYTE_IDE_REG(0x1f4);
-	sb_ide_hwif->hw.io_ports[IDE_HCYL_OFFSET]    = SIBYTE_IDE_REG(0x1f5);
-	sb_ide_hwif->hw.io_ports[IDE_SELECT_OFFSET]  = SIBYTE_IDE_REG(0x1f6);
-	sb_ide_hwif->hw.io_ports[IDE_STATUS_OFFSET]  = SIBYTE_IDE_REG(0x1f7);
-	sb_ide_hwif->hw.io_ports[IDE_CONTROL_OFFSET] = SIBYTE_IDE_REG(0x3f6);
-	sb_ide_hwif->hw.io_ports[IDE_IRQ_OFFSET]     = SIBYTE_IDE_REG(0x3f7);
-
-	sb_ide_hwif->hw.irq                          = K_INT_GB_IDE;
-	sb_ide_hwif->irq			     = K_INT_GB_IDE;
-	sb_ide_hwif->hw.ack_intr		     = NULL;
-	sb_ide_hwif->noprobe			     = 0;
-
-	memcpy(sb_ide_hwif->io_ports, sb_ide_hwif->hw.io_ports, sizeof(sb_ide_hwif->io_ports));
+	base = ioremap(A_IO_EXT_BASE, 0x800);
+	offset = bus_readq(base + R_IO_EXT_REG(R_IO_EXT_START_ADDR, IDE_CS));
+	size = bus_readq(base + R_IO_EXT_REG(R_IO_EXT_MULT_SIZE, IDE_CS));
+	iounmap(base);
 	
-	printk("SiByte onboard IDE configured as device %d\n", i);
+	offset = G_IO_START_ADDR(offset) << S_IO_ADDRBASE;
+	size = (G_IO_MULT_SIZE(size) + 1) << S_IO_REGSIZE;
+	if (offset < A_PHYS_GENBUS || offset >= A_PHYS_GENBUS_END) {
+		printk(KERN_INFO DRV_NAME
+		       ": IDE interface at GenBus disabled\n");
+		return;
+	}
 
-	/* Prevent resource map manipulation */
-	sb_ide_hwif->mmio = 2;
+	printk(KERN_INFO DRV_NAME ": IDE interface at GenBus slot %i\n",
+	       IDE_CS);
 
-	/* Reset the ideops */
-	sibyte_set_ideops(sb_ide_hwif);
-#endif
+	swarm_ide_resource.start = offset;
+	swarm_ide_resource.end = offset + size - 1;
+	if (request_resource(&iomem_resource, &swarm_ide_resource)) {
+		printk(KERN_ERR DRV_NAME
+		       ": can't request I/O memory resource\n");
+		return;
+	}
+
+	base = ioremap(offset, size);
+
+	/* Setup MMIO ops.  */
+	default_hwif_mmiops(hwif);
+	/* Prevent resource map manipulation.  */
+	hwif->mmio = 2;
+	hwif->noprobe = 0;
+
+	for (i = IDE_DATA_OFFSET; i <= IDE_STATUS_OFFSET; i++)
+		hwif->hw.io_ports[i] =
+				(unsigned long)(base + ((0x1f0 + i) << 5));
+	hwif->hw.io_ports[IDE_CONTROL_OFFSET] =
+				(unsigned long)(base + (0x3f6 << 5));
+	hwif->hw.irq = K_INT_GB_IDE;
+
+	memcpy(hwif->io_ports, hwif->hw.io_ports, sizeof(hwif->io_ports));
+	hwif->irq = hwif->hw.irq;
 }
-
