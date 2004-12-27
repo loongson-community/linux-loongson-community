930420441ceee03757762c388e3dba0365f0a48c
Ralf Baechle <ralf@linux-mips.org>
2004-12-27 12:00:10 +0000

    Export probe_irq_mask.

--- arch/mips/kernel/mips_ksyms.c
+++ arch/mips/kernel/mips_ksyms.c
@@ -8,6 +8,7 @@
  * Copyright (C) 1996, 97, 98, 99, 2000, 01, 03 by Ralf Baechle
  * Copyright (C) 1999, 2000, 01 Silicon Graphics, Inc.
  */
+#include <linux/interrupt.h>
 #include <linux/module.h>
 #include <asm/checksum.h>
 #include <asm/pgtable.h>
@@ -60,3 +61,6 @@ EXPORT_SYMBOL(__strnlen_user_asm);
 EXPORT_SYMBOL(csum_partial);
 
 EXPORT_SYMBOL(invalid_pte_table);
+#ifdef CONFIG_GENERIC_IRQ_PROBE
+EXPORT_SYMBOL(probe_irq_mask);
+#endif
