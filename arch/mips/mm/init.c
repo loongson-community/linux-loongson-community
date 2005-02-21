97b0bc44b0e639364308420c2ea6dd14f4f9f83c
Thiemo Seufer <ths@networkno.de>
2005-02-21 10:56:34 +0000

    Fix compiler warning.

--- arch/mips/mm/init.c
+++ arch/mips/mm/init.c
@@ -86,7 +86,7 @@ EXPORT_SYMBOL(kmap_prot);
 EXPORT_SYMBOL(kmap_pte);
 
 #define kmap_get_fixmap_pte(vaddr)					\
-	pte_offset_kernel(pmd_offset(pgd_offset_k(vaddr), (vaddr)), (vaddr))
+	pte_offset_kernel(pmd_offset(pud_offset(pgd_offset_k(vaddr), (vaddr)), (vaddr)), (vaddr))
 
 static void __init kmap_init(void)
 {
