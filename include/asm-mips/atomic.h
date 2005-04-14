7bb6aca88547197d5245da0de3f4460367d23050
Ralf Baechle <ralf@linux-mips.org>
2005-04-14 18:23:28 +0000

    Pull previous patch.  Gcc doesn't share your opinion, Thiemo ;-)

--- include/asm-mips/atomic.h
+++ include/asm-mips/atomic.h
@@ -67,7 +67,7 @@ static __inline__ void atomic_add(int i, atomic_t * v)
 		"	sc	%0, %1					\n"
 		"	beqzl	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -77,7 +77,7 @@ static __inline__ void atomic_add(int i, atomic_t * v)
 		"	sc	%0, %1					\n"
 		"	beqz	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else {
 		unsigned long flags;
 
@@ -105,7 +105,7 @@ static __inline__ void atomic_sub(int i, atomic_t * v)
 		"	sc	%0, %1					\n"
 		"	beqzl	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -115,7 +115,7 @@ static __inline__ void atomic_sub(int i, atomic_t * v)
 		"	sc	%0, %1					\n"
 		"	beqz	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else {
 		unsigned long flags;
 
@@ -143,7 +143,8 @@ static __inline__ int atomic_add_return(int i, atomic_t * v)
 		"	addu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -155,7 +156,8 @@ static __inline__ int atomic_add_return(int i, atomic_t * v)
 		"	addu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
@@ -184,7 +186,8 @@ static __inline__ int atomic_sub_return(int i, atomic_t * v)
 		"	subu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -196,7 +199,8 @@ static __inline__ int atomic_sub_return(int i, atomic_t * v)
 		"	subu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
@@ -233,7 +237,8 @@ static __inline__ int atomic_sub_if_positive(int i, atomic_t * v)
 		"	sync						\n"
 		"1:							\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -246,7 +251,8 @@ static __inline__ int atomic_sub_if_positive(int i, atomic_t * v)
 		"	sync						\n"
 		"1:							\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
@@ -366,7 +372,7 @@ static __inline__ void atomic64_add(long i, atomic64_t * v)
 		"	scd	%0, %1					\n"
 		"	beqzl	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -376,7 +382,7 @@ static __inline__ void atomic64_add(long i, atomic64_t * v)
 		"	scd	%0, %1					\n"
 		"	beqz	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else {
 		unsigned long flags;
 
@@ -404,7 +410,7 @@ static __inline__ void atomic64_sub(long i, atomic64_t * v)
 		"	scd	%0, %1					\n"
 		"	beqzl	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -414,7 +420,7 @@ static __inline__ void atomic64_sub(long i, atomic64_t * v)
 		"	scd	%0, %1					\n"
 		"	beqz	%0, 1b					\n"
 		: "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "1" (v->counter));
+		: "Ir" (i), "m" (v->counter));
 	} else {
 		unsigned long flags;
 
@@ -442,7 +448,8 @@ static __inline__ long atomic64_add_return(long i, atomic64_t * v)
 		"	addu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -454,7 +461,8 @@ static __inline__ long atomic64_add_return(long i, atomic64_t * v)
 		"	addu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
@@ -483,7 +491,8 @@ static __inline__ long atomic64_sub_return(long i, atomic64_t * v)
 		"	subu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -495,7 +504,8 @@ static __inline__ long atomic64_sub_return(long i, atomic64_t * v)
 		"	subu	%0, %1, %3				\n"
 		"	sync						\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
@@ -532,7 +542,8 @@ static __inline__ long atomic64_sub_if_positive(long i, atomic64_t * v)
 		"	sync						\n"
 		"1:							\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else if (cpu_has_llsc) {
 		unsigned long temp;
 
@@ -545,7 +556,8 @@ static __inline__ long atomic64_sub_if_positive(long i, atomic64_t * v)
 		"	sync						\n"
 		"1:							\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
-		: "Ir" (i), "2" (v->counter));
+		: "Ir" (i), "m" (v->counter)
+		: "memory");
 	} else {
 		unsigned long flags;
 
