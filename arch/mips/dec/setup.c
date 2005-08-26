9f56d379776e36ef1546f532f2966b80b6cb9b5f
Maciej W. Rozycki <macro@linux-mips.org>
2005-08-26 13:13:29 +0000

    Fix an incomplete rework.

--- arch/mips/dec/setup.c
+++ arch/mips/dec/setup.c
@@ -148,7 +148,7 @@ void __init dec_be_init(void)
 extern void dec_time_init(void);
 extern void dec_timer_setup(struct irqaction *);
 
-void __init decstation_setup(void)
+void __init plat_setup(void)
 {
 	board_be_init = dec_be_init;
 	board_time_init = dec_time_init;
