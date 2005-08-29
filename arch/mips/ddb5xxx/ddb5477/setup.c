7c5688fa9c2be699ac9a3885b77ff4f834995049
Ralf Baechle <ralf@linux-mips.org>
2005-08-29 16:52:43 +0000

    plat_setup conversion for DDB5477.

--- arch/mips/ddb5xxx/ddb5477/setup.c
+++ arch/mips/ddb5xxx/ddb5477/setup.c
@@ -170,7 +170,7 @@ static void ddb5477_board_init(void);
 extern struct pci_controller ddb5477_ext_controller;
 extern struct pci_controller ddb5477_io_controller;
 
-static void ddb5477_setup(void)
+void __init plat_setup(void)
 {
 	/* initialize board - we don't trust the loader */
         ddb5477_board_init();
