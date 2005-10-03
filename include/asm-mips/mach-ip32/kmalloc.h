7c598df35a43f53dd6704bb2490f82fcd1e28a9a
Ralf Baechle <ralf@linux-mips.org>
2005-10-03 15:46:12 +0100

    Fix typo.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-ip32/kmalloc.h
+++ include/asm-mips/mach-ip32/kmalloc.h
@@ -1,7 +1,7 @@
 #ifndef __ASM_MACH_IP32_KMALLOC_H
 #define __ASM_MACH_IP32_KMALLOC_H
 
-#if defined(CONFIG_CPU_R5000) || defined defined (CONFIG_CPU_RM7000)
+#if defined(CONFIG_CPU_R5000) || defined (CONFIG_CPU_RM7000)
 #define ARCH_KMALLOC_MINALIGN	32
 #else
 #define ARCH_KMALLOC_MINALIGN	128
