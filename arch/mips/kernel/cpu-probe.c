b0e05a32a745a6e3ec5203f28a6bc044653e411a
Ralf Baechle <ralf@linux-mips.org>
2007-06-21 00:22:34 +0100

    [MIPS] Fix scheduling latency issue on 24K, 34K and 74K cores
    The idle loop goes to sleep using the WAIT instruction if !need_resched().
    This has is suffering from from a race condition that if if just after
    need_resched has returned 0 an interrupt might set TIF_NEED_RESCHED but
    we've just completed the test so go to sleep anyway.  This would be
    trivial to fix by just disabling interrupts during that sequence as in:
    
            local_irq_disable();
            if (!need_resched())
                    __asm__("wait");
            local_irq_enable();
    
    but the processor architecture leaves it undefined if a processor calling
    WAIT with interrupts disabled will ever restart its pipeline and indeed
    some processors have made use of the freedom provided by the architecture
    definition.  This has been resolved and the Config7.WII bit indicates that
    the use of WAIT is safe on 24K, 24KE and 34K cores.  It also is safe on
    74K EA so enable the use of WAIT with interrupts disabled for all 74K
    cores.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/cpu-probe.c
+++ arch/mips/kernel/cpu-probe.c
@@ -137,13 +137,19 @@ static inline void check_wait(void)
 	case CPU_4KEC:
 	case CPU_4KSC:
 	case CPU_5KC:
-	case CPU_24K:
 	case CPU_25KF:
-	case CPU_34K:
-	case CPU_74K:
  	case CPU_PR4450:
 		cpu_wait = r4k_wait;
 		break;
+
+	case CPU_24K:
+	case CPU_34K:
+		cpu_wait = r4k_wait;
+		if (read_c0_config7() & MIPS_CONF7_WII)
+			cpu_wait = r4k_wait_irqoff;
+		break;
+
+	case CPU_74K:
 	case CPU_TX49XX:
 		cpu_wait = r4k_wait_irqoff;
 		break;
