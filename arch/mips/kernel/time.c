a7edc20b82574692de225755143a3bd8a0dbe1d4
Ralf Baechle <ralf@linux-mips.org>
2007-10-19 09:18:39 +0100

    [MIPS] time: New function to compute clocksource shift and mult.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/time.c
+++ arch/mips/kernel/time.c
@@ -187,6 +187,23 @@ void __init clocksource_set_clock(struct clocksource *cs, unsigned int clock)
 	cs->mult = (u32) temp;
 }
 
+void __cpuinit clockevent_set_clock(struct clock_event_device *cd,
+	unsigned int clock)
+{
+	u64 temp;
+	u32 shift;
+
+	/* Find a shift value */
+	for (shift = 32; shift > 0; shift--) {
+		temp = (u64) NSEC_PER_SEC << shift;
+		do_div(temp, clock);
+		if ((temp >> 32) == 0)
+			break;
+	}
+	cd->shift = shift;
+	cd->mult = (u32) temp;
+}
+
 static void __init init_mips_clocksource(void)
 {
 	if (!mips_hpt_frequency || clocksource_mips.read == null_hpt_read)
