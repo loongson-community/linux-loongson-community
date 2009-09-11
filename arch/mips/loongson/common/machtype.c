f1411fa921a9f6be46e34758b450242f91be10a1
Wu Zhangjin <wuzhangjin@gmail.com>
2009-09-11 13:05:45 +0800

    [loongson] add NAS support
    This patch add the support to Lemote's Loongson-2F based network
    attached system.
    
    The kernel support to this machine is almost the same as fuloong2f, the
    only difference is that it use the serial port provided by loongson2f
    processor as yeeloong2f does.
    
    If you want to enable this NAS support, you need to use the default
    kernel configuration file:
    
    arch/mips/configs/fuloong2f_unas_defconfig
    
    This configuration is based on the old arch/mips/configs/fuloong2f_defconfig,
    but tuned by Cheng Renquan:
    
    1) many unrelated drivers disabled,
    2) unas special drivers inlined, like sata_sil for sata sil 3114
       pci card, and mtd drivers for unas NOR flash,
    3) some default options like slab => slub switch has been done
       years ago, during 2.6.23 developing,
    4) some old kernel features disabled and some promising new
       technologies modularized, like squashfs, btrfs, and nilfs,
    5) ipv6 and SECURITY options disabled, this may be controversial,
       and need to be discussed further
    
    Thanks goes to him.
    
    Tested-by: Cheng Renquan <crquan@gmail.com>
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Signed-off-by: Cheng Renquan <crquan@gmail.com>

--- arch/mips/loongson/common/machtype.c
+++ arch/mips/loongson/common/machtype.c
@@ -22,6 +22,7 @@ static const char *system_types[] = {
 	[MACH_LEMOTE_ML2F7]             "lemote-mengloong-2f-7inches",
 	[MACH_LEMOTE_YL2F89]            "lemote-yeeloong-2f-8.9inches",
 	[MACH_DEXXON_GDIUM2F10]         "dexxon-gidum-2f-10inches",
+	[MACH_LEMOTE_FL2FNAS]           "lemote-fuloong-2f-nas",
 	[MACH_LOONGSON_END]             NULL,
 };
 
