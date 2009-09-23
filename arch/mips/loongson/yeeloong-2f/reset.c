f448377f597b6505e1de674901d426e9e1a0c34e
Wu Zhangjin <wuzhangjin@gmail.com>
2009-09-23 15:25:09 +0800

    [loongson] yeeloong2f: cleanup the reset logic with ec_write function
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/loongson/yeeloong-2f/reset.c
+++ arch/mips/loongson/yeeloong-2f/reset.c
@@ -17,19 +17,7 @@
 #include <loongson.h>
 #include <machine.h>
 
-/*
- * The following registers are determined by the EC index configuration.
- * 1. fill the PORT_HIGH as EC register high part.
- * 2. fill the PORT_LOW as EC register low part.
- * 3. fill the PORT_DATA as EC register write data or get the data from it.
- */
-
-#define	EC_IO_PORT_HIGH	0x0381
-#define	EC_IO_PORT_LOW	0x0382
-#define	EC_IO_PORT_DATA	0x0383
-#define	REG_RESET_HIGH	0xF4	/* reset the machine auto-clear : rd/wr */
-#define REG_RESET_LOW	0xEC
-#define	BIT_RESET_ON	(1 << 0)
+#include <ec/kb3310b.h>
 
 /* menglong(7inches) laptop has different shutdown logic from 8.9inches */
 #define EC_SHUTDOWN_IO_PORT_HIGH 0xff2d
@@ -48,11 +36,7 @@ void mach_prepare_reboot(void)
 	LOONGSON_CHIPCFG0 |= 0x7;
 
 	/* sending an reset signal to EC(embedded controller) */
-	outb(REG_RESET_HIGH, EC_IO_PORT_HIGH);
-	outb(REG_RESET_LOW, EC_IO_PORT_LOW);
-	mmiowb();
-	outb(BIT_RESET_ON, EC_IO_PORT_DATA);
-	mmiowb();
+	ec_write(REG_RESET, BIT_RESET_ON);
 }
 
 void mach_prepare_shutdown(void)
