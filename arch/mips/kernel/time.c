4e32165c1968f8bac69de241bf11b611e4a1dc0c
Wu Zhangjin <wuzhangjin@gmail.com>
2010-01-30 03:49:38 +0800

    MIPS: Cleanup of sched_clock()
    "This patch ensures the sched_clock() calculate from ZERO to make the
    PRINTK_TIME=y works normally." -- we only need to do this when the
    CONFIG_HR_SCHED_CLOCK is enabled.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    
    Conflicts:
    
    	arch/mips/kernel/time.c
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/kernel/time.c
+++ arch/mips/kernel/time.c
@@ -119,8 +119,10 @@ static __init int cpu_has_mfc0_count_bug(void)
 
 void __init time_init(void)
 {
+#ifdef CONFIG_HR_SCHED_CLOCK
 	if (!mips_clockevent_init() || !cpu_has_mfc0_count_bug())
 		write_c0_count(0);
+#endif
 
 	plat_time_init();
 
