4b202f7327f2e6d6ae1157de0332ce69bca5fd16
Wu Zhangjin <wuzhangjin@gmail.com>
2010-07-18 15:17:57 +0800

    MIPS: Loongson: Remove set_irq_trigger_mode()
    set_irq_trigger_mode() is not needed by all of the platforms, remove it
    and put the related sourc code into mach_init_irq().
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/include/asm/mach-loongson/loongson.h
+++ arch/mips/include/asm/mach-loongson/loongson.h
@@ -51,7 +51,6 @@ extern char loongson_cmdline[COMMAND_LINE_SIZE];
 /* irq operation functions */
 extern void bonito_irqdispatch(void);
 extern void __init bonito_irq_init(void);
-extern void __init set_irq_trigger_mode(void);
 extern void __init mach_init_irq(void);
 extern void mach_irq_dispatch(unsigned int pending);
 extern int mach_i8259_irq(void);
