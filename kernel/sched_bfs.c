5c293fd5fcc2bd7732e5f672bee5686345908aca
Wu Zhangjin <wuzhangjin@gmail.com>
2010-10-25 00:40:29 +0800

    sched: add bfs357-worker_fix.patch
    Apples the fix from
    http://ck.kolivas.org/patches/bfs/2.6.36/bfs357-worker_fix.patch
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -1460,7 +1460,6 @@ static void try_to_wake_up_local(struct task_struct *p)
 	struct rq *rq = task_rq(p);
 	bool success = false;
 
-	BUG_ON(rq != this_rq());
 	BUG_ON(p == current);
 	lockdep_assert_held(&grq.lock);
 
