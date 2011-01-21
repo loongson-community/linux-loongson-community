2ba694ee20d69a64f76721db4bf3e6d3303fd050
Wu Zhangjin <wuzhangjin@gmail.com>
2011-01-22 01:43:33 +0800

    MIPS: Loongson: Fix the serial port and early printk support
    This decouple the serial port support and the early printk support and
    fixes the serial port for different Loongson boards.
    
    Signed-off-by: Liu Shiwei <liushiwei@anheng.com.cn>
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/include/asm/mach-loongson/loongson.h
+++ arch/mips/include/asm/mach-loongson/loongson.h
@@ -31,17 +31,7 @@ extern void __init prom_init_memory(void);
 extern void __init prom_init_cmdline(void);
 extern void __init prom_init_machtype(void);
 extern void __init prom_init_env(void);
-#ifdef CONFIG_LOONGSON_UART_BASE
-extern unsigned long _loongson_uart_base, loongson_uart_base;
-extern void prom_init_loongson_uart_base(void);
-#endif
-
-static inline void prom_init_uart_base(void)
-{
-#ifdef CONFIG_LOONGSON_UART_BASE
-	prom_init_loongson_uart_base();
-#endif
-}
+extern void __init prom_init_uart_base(void);
 
 /*
  * Copy kernel command line from arcs_cmdline
