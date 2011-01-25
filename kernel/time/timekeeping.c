8e8d6e75bb3c010933cf9d6ef513fd4dff302bce
Wu Zhangjin <wuzhangjin@gmail.com>
2011-01-25 23:20:48 +0800

    sysfs, pm: Remove resume,suspend If !CONFIG_SYSFS || !CONFIG_PM
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -509,6 +509,8 @@ void __init timekeeping_init(void)
 	write_sequnlock_irqrestore(&xtime_lock, flags);
 }
 
+#if defined(CONFIG_SYSFS) && defined(CONFIG_PM)
+
 /* time in seconds when suspend began */
 static struct timespec timekeeping_suspend_time;
 
@@ -591,6 +593,7 @@ static int __init timekeeping_init_device(void)
 }
 
 device_initcall(timekeeping_init_device);
+#endif	/* CONFIG_SYSFS && CONFIG_PM */
 
 /*
  * If the error is already larger, we look ahead even further
